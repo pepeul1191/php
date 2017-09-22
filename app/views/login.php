9e3dabd7957f1688604c7600967d1a7d00127951
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-22 08:42:19 -0500

    Pantalla de login

--- app/views/login.php
+++ app/views/login.php
@@ -2,12 +2,21 @@
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" href="<?php echo Configuration::get('base_url');?>public/bower_components/bootstrap/dist/css/bootstrap.min.css" /
+    <link rel="stylesheet" href="<?php echo Configuration::get('base_url');?>public/bower_components/bootstrap/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo Configuration::get('base_url');?>public/bower_components/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="<?php echo Configuration::get('base_url');?>public/assets/login.css" />
 </head>
 <body>
-    <h1>Hola</h1>
-    <h3><?php echo Configuration::get('base_url');?></h3>
+    <div class="login-page">
+        <div class="form">
+            <form class="login-form">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+                <p class="message">Not registered? <a href="#">Create an account</a></p>
+            </form>
+        </div>
+    </div>
     <script type="text/javascript" src="<?php echo Configuration::get('base_url');?>public/bower_components/jquery/dist/jquery.min.js"></script>
 </body>
 </html>
