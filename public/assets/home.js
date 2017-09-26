3d8a57358289f41eadac687d55574dc8071f8b32
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-26 07:04:25 -0500

    Más operaciones por fila del crud de ubiacaciones

--- public/assets/home.js
+++ public/assets/home.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	   		var rpta =  '<thead><tr><th>id</th><th>Nombre</th><th>Operaciones</th></tr></thead>';
 	   		for (var i = 0; i < departamentos.length; i++) {
 			  	rpta = rpta + '<tr>';
-			  	var operaciones = '<td><i class="fa fa-pencil" aria-hidden="true" operacion="cargarProvincia"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarDepartamento"></i></td>';
+			  	var operaciones = '<td><i class="fa fa-search" aria-hidden="true" operacion="cargarProvincia"></i><i class="fa fa-pencil" aria-hidden="true" operacion="editarDepartamento"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarDepartamento"></i></td>';
 			  	rpta = rpta + '<td>' + departamentos[i]['id'] + '</td><td>' + '<input type="text" value="'+ departamentos[i]['nombre'] + '">' +'</td>' + operaciones;
 			  	rpta = rpta + '</tr>';
 			}
@@ -25,31 +25,34 @@ $(document).on('click', '.fa', function(event) {
   	var operacion = $(event.currentTarget).attr('operacion');
   	
   	switch(operacion) {
+  		//Inicio tabla departamento
 	    case 'cargarProvincia':
 	   		var departamentoId = $(event.currentTarget).parent().parent().children().eq(0).html();
 	   		cargarProvincia(departamentoId);
 			break;
 	    case 'eliminarDepartamento':
-
+	    	
 			break;
 		case 'editarDepartamento':
 
 			break;
+		//Inicio tabla provincia
 		case 'eliminarProvincia':
 
 			break;
 		case 'editarProvincia':
 
 			break;
+		case 'cargarDistrito':
+	   		var distritoId = $(event.currentTarget).parent().parent().children().eq(0).html();
+	   		cargarDistrito(distritoId);
+			break;
+		//Inicio tabla distrito
 		case 'eliminarDistrito':
 
 			break;
 		case 'editarDistrito':
 
-			break;
-		case 'cargarDistrito':
-	   		var distritoId = $(event.currentTarget).parent().parent().children().eq(0).html();
-	   		cargarDistrito(distritoId);
 			break;
 	    default:
 	       alert('Operacion ' + operacion + ' no implementada');
@@ -70,7 +73,7 @@ function cargarProvincia(departamentoId){
 	   		var rpta =  '<thead><tr><th>id</th><th>Nombre</th><th>Operaciones</th></tr></thead>';
 	   		for (var i = 0; i < provincias.length; i++) {
 			  	rpta = rpta + '<tr>';
-			  	var operaciones = '<td><i class="fa fa-pencil" aria-hidden="true" operacion="cargarDistrito"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarProvincia"></i></td>';
+			  	var operaciones = '<td><i class="fa fa-search" aria-hidden="true" operacion="cargarDistrito"></i><i class="fa fa-pencil" aria-hidden="true" operacion="editarProvincia"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarProvincia"></i></td>';
 			  	rpta = rpta + '<td>' + provincias[i]['id'] + '</td><td>' + '<input type="text" value="'+ provincias[i]['nombre'] + '">' +'</td>' + operaciones;
 			  	rpta = rpta + '</tr>';
 			}
@@ -94,7 +97,7 @@ function cargarDistrito(provinciaId){
 	   		var rpta =  '<thead><tr><th>id</th><th>Nombre</th><th>Operaciones</th></tr></thead>';
 	   		for (var i = 0; i < distritos.length; i++) {
 			  	rpta = rpta + '<tr>';
-			  	var operaciones = '<td style="padding-left:20px;"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarDistrito"></i></td>';
+			  	var operaciones = '<td style="padding-left:20px;"><i class="fa fa-pencil" aria-hidden="true" operacion="editarDistrito"></i><i class="fa fa-times" aria-hidden="true" operacion="eliminarDistrito"></i></td>';
 			  	rpta = rpta + '<td>' + distritos[i]['id'] + '</td><td>' + '<input type="text" value="'+ distritos[i]['nombre'] + '">' +'</td>' + operaciones;
 			  	rpta = rpta + '</tr>';
 			}
