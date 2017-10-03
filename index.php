4ca714e8a9f0fd214cd2a4689f9471a5509df9f7
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-02 21:23:37 -0500

    Corrigiendo cors

--- index.php
+++ index.php
@@ -31,24 +31,24 @@ Flight::route('GET /error/404', array('ErrorController','error_404'));
 Flight::route('GET /login', array('ParcialController','login'));
 Flight::route('GET /home', array('ParcialController','home'));
 #LOGIN
-Flight::route('POST /usuario/correo_repetido', array('UsuarioController','correo_repetido'));
-Flight::route('POST /usuario/usuario_repetido', array('UsuarioController','usuario_repetido'));
-Flight::route('POST /cipher/encode', array('CipherController','encode'));
-Flight::route('POST /usuario/guardar', array('UsuarioController','guardar'));
-Flight::route('POST /usuario/validar', array('UsuarioController','validar'));
+Flight::route('OPTIONS|POST /usuario/correo_repetido', array('UsuarioController','correo_repetido'));
+Flight::route('OPTIONS|POST /usuario/usuario_repetido', array('UsuarioController','usuario_repetido'));
+Flight::route('OPTIONS|POST /cipher/encode', array('CipherController','encode'));
+Flight::route('OPTIONS|POST /usuario/guardar', array('UsuarioController','guardar'));
+Flight::route('OPTIONS|POST /usuario/validar', array('UsuarioController','validar'));
 #UBICACIONES
 Flight::route('GET /departamento/listar', array('DepartamentoController','listar'));
-Flight::route('POST /departamento/crear', array('DepartamentoController','crear'));
-Flight::route('POST /departamento/editar', array('DepartamentoController','editar'));
-Flight::route('POST /departamento/eliminar', array('DepartamentoController','eliminar'));
+Flight::route('OPTIONS|POST /departamento/crear', array('DepartamentoController','crear'));
+Flight::route('OPTIONS|POST /departamento/editar', array('DepartamentoController','editar'));
+Flight::route('OPTIONS|POST /departamento/eliminar', array('DepartamentoController','eliminar'));
 Flight::route('GET /provincia/listar/@departamento_id', array('ProvinciaController','listar'));
-Flight::route('POST /provincia/crear', array('ProvinciaController','crear'));
-Flight::route('POST /provincia/editar', array('ProvinciaController','editar'));
-Flight::route('POST /provincia/eliminar', array('ProvinciaController','eliminar'));
+Flight::route('OPTIONS|POST /provincia/crear', array('ProvinciaController','crear'));
+Flight::route('OPTIONS|POST /provincia/editar', array('ProvinciaController','editar'));
+Flight::route('OPTIONS|POST /provincia/eliminar', array('ProvinciaController','eliminar'));
 Flight::route('GET /distrito/listar/@provincia_id', array('DistritoController','listar'));
-Flight::route('POST /distrito/crear', array('DistritoController','crear'));
-Flight::route('POST /distrito/editar', array('DistritoController','editar'));
-Flight::route('POST /distrito/eliminar', array('DistritoController','eliminar'));
+Flight::route('OPTIONS|POST /distrito/crear', array('DistritoController','crear'));
+Flight::route('OPTIONS|POST /distrito/editar', array('DistritoController','editar'));
+Flight::route('OPTIONS|POST /distrito/eliminar', array('DistritoController','eliminar'));
 
 /*
 Flight::map('notFound', function(){
