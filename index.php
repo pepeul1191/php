97e6fa69edcec07afda8df34c0063ac00063b03b
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-02 21:24:48 -0500

    Correción del commit anterior

--- index.php
+++ index.php
@@ -27,28 +27,45 @@ Flight::route('GET /error/404', array('ErrorController','error_404'));
 //Flight::route('POST /encode', array('CipherController','encode'));
 //Flight::route('POST /decode', array('CipherController','decode'));
 
+
+
+Flight::route('OPTIONS /usuario/correo_repetido', array('UsuarioController','correo_repetido'));
+Flight::route('OPTIONS /usuario/usuario_repetido', array('UsuarioController','usuario_repetido'));
+Flight::route('OPTIONS /cipher/encode', array('CipherController','encode'));
+Flight::route('OPTIONS /usuario/guardar', array('UsuarioController','guardar'));
+Flight::route('OPTIONS /usuario/validar', array('UsuarioController','validar'));
+Flight::route('OPTIONS /departamento/crear', array('DepartamentoController','crear'));
+Flight::route('OPTIONS /departamento/editar', array('DepartamentoController','editar'));
+Flight::route('OPTIONS /departamento/eliminar', array('DepartamentoController','eliminar'));
+Flight::route('OPTIONS /provincia/crear', array('ProvinciaController','crear'));
+Flight::route('OPTIONS /provincia/editar', array('ProvinciaController','editar'));
+Flight::route('OPTIONS /provincia/eliminar', array('ProvinciaController','eliminar'));
+Flight::route('OPTIONS /distrito/crear', array('DistritoController','crear'));
+Flight::route('OPTIONS /distrito/editar', array('DistritoController','editar'));
+Flight::route('OPTIONS /distrito/eliminar', array('DistritoController','eliminar'));
+
 #VISTAS
 Flight::route('GET /login', array('ParcialController','login'));
 Flight::route('GET /home', array('ParcialController','home'));
 #LOGIN
-Flight::route('OPTIONS|POST /usuario/correo_repetido', array('UsuarioController','correo_repetido'));
-Flight::route('OPTIONS|POST /usuario/usuario_repetido', array('UsuarioController','usuario_repetido'));
-Flight::route('OPTIONS|POST /cipher/encode', array('CipherController','encode'));
-Flight::route('OPTIONS|POST /usuario/guardar', array('UsuarioController','guardar'));
-Flight::route('OPTIONS|POST /usuario/validar', array('UsuarioController','validar'));
+Flight::route('POST /usuario/correo_repetido', array('UsuarioController','correo_repetido'));
+Flight::route('POST /usuario/usuario_repetido', array('UsuarioController','usuario_repetido'));
+Flight::route('POST /cipher/encode', array('CipherController','encode'));
+Flight::route('POST /usuario/guardar', array('UsuarioController','guardar'));
+Flight::route('POST /usuario/validar', array('UsuarioController','validar'));
 #UBICACIONES
 Flight::route('GET /departamento/listar', array('DepartamentoController','listar'));
-Flight::route('OPTIONS|POST /departamento/crear', array('DepartamentoController','crear'));
-Flight::route('OPTIONS|POST /departamento/editar', array('DepartamentoController','editar'));
-Flight::route('OPTIONS|POST /departamento/eliminar', array('DepartamentoController','eliminar'));
+Flight::route('POST /departamento/crear', array('DepartamentoController','crear'));
+Flight::route('POST /departamento/editar', array('DepartamentoController','editar'));
+Flight::route('POST /departamento/eliminar', array('DepartamentoController','eliminar'));
 Flight::route('GET /provincia/listar/@departamento_id', array('ProvinciaController','listar'));
-Flight::route('OPTIONS|POST /provincia/crear', array('ProvinciaController','crear'));
-Flight::route('OPTIONS|POST /provincia/editar', array('ProvinciaController','editar'));
-Flight::route('OPTIONS|POST /provincia/eliminar', array('ProvinciaController','eliminar'));
+Flight::route('POST /provincia/crear', array('ProvinciaController','crear'));
+Flight::route('POST /provincia/editar', array('ProvinciaController','editar'));
+Flight::route('POST /provincia/eliminar', array('ProvinciaController','eliminar'));
 Flight::route('GET /distrito/listar/@provincia_id', array('DistritoController','listar'));
-Flight::route('OPTIONS|POST /distrito/crear', array('DistritoController','crear'));
-Flight::route('OPTIONS|POST /distrito/editar', array('DistritoController','editar'));
-Flight::route('OPTIONS|POST /distrito/eliminar', array('DistritoController','eliminar'));
+Flight::route('POST /distrito/crear', array('DistritoController','crear'));
+Flight::route('POST /distrito/editar', array('DistritoController','editar'));
+Flight::route('POST /distrito/eliminar', array('DistritoController','eliminar'));
 
 /*
 Flight::map('notFound', function(){
